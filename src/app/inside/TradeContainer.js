752e18ce38f99cd0bf11309d17f1a962ceeea26e
prochao <prochaos18@gmail.com>
2024-10-02 20:47:49 +0530

    added max

--- src/app/inside/TradeContainer.js
+++ src/app/inside/TradeContainer.js
@@ -139,7 +139,7 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
     abi: TOKEN_COIN_ABI,
     address: POOL_ADDRESS,
     functionName: "getBuyTokens",
-    args: [projectId, projects?.[7], (((buyValue - (buyValue*0.01)) *1e18) || 0)],
+    args: [projectId, projects?.[7], buyValue == "0" ? "0" : buyValue*1e18],
   });
   
 
@@ -148,21 +148,21 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
     abi: TOKEN_COIN_ABI,
     address: POOL_ADDRESS,
     functionName: "getSellTokens",
-    args: [projectId, projects?.[7], (sellValue > 0.01 ? sellValue - 0.01 : sellValue)*1e18],
+    args: [projectId, projects?.[7], sellValue == "0" ? "0" : sellValue*1e18],
   });
 
   const { data: getBuyFee , queryKey: getBuyFeeQueryKey } = useReadContract({
     abi: TOKEN_COIN_ABI,
     address: POOL_ADDRESS,
     functionName: "getBuyFee",
-    args: [ buyValue == "0" ? "0" : (((buyValue - (buyValue*0.01)) *1e18) || 0), projects?.[7]],
+    args: [ buyValue == "0" ? "0" : buyValue*1e18, projects?.[7]],
   });
 
   const { data: getSellFee , queryKey: getSellFeeQueryKey } = useReadContract({
     abi: TOKEN_COIN_ABI,
     address: POOL_ADDRESS,
     functionName: "getSellFee",
-    args: [sellValue == "0" ? "0" : (sellValue > 0.01 ? sellValue - 0.01 : sellValue)*1e18, projects?.[7]],
+    args: [sellValue == "0" ? "0" : sellValue*1e18, projects?.[7]],
   });
 
 
@@ -202,7 +202,7 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
       address: POOL_ADDRESS,
       abi: TOKEN_COIN_ABI,
       functionName: "buyTokens",
-      args: [projectId, parseEther(((buyValue - (buyValue*0.01))).toString()),slippage*100, getTokensForAmount?.[0]],
+      args: [projectId, parseEther(buyValue.toString()),slippage*100, getTokensForAmount?.[0]],
       value:
         projects?.[7] === false
           ? parseEther(buyValue.toString()) + getBuyFee
@@ -243,7 +243,7 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
       address: POOL_ADDRESS,
       abi: TOKEN_COIN_ABI,
       functionName: "sellTokens",
-      args: [projectId, parseFloat(formatEther(tokenBalance) * 1).toFixed(6) == sellValue ? tokenBalance : parseEther((sellValue - 0.01)?.toString()) , slippage*100 , getSellTokens?.[0] ],
+      args: [projectId, parseFloat(formatEther(tokenBalance) * 1).toFixed(6) == sellValue ? tokenBalance : parseEther(sellValue?.toString()) , slippage*100 , getSellTokens?.[0] ],
       value: 0,
     };
     setSellTxn(null);
@@ -449,7 +449,7 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
           <div className="d-flex align-items-center gap-2">
             {!show ? <button className="btn1" onClick={() => setBuyValue(tokenData?.isToken == true
               ? formatEther(alternateTokenBalance || 0)
-              : parseFloat(balance?.value > (TOTAL_RAISE -  projects?.[6]) ? formatEther((TOTAL_RAISE -  projects?.[6]) || 0) : formatEther(balance?.value || 0)).toFixed(18))}>Max</button> : null}
+              : parseFloat(balance?.value > (TOTAL_RAISE -  projects?.[6]) ? formatEther((TOTAL_RAISE -  projects?.[6]) || 0) : parseFloat(formatEther(balance?.value || 0) - (formatEther(balance?.value || 0)*0.02) )).toFixed(18))}>Max</button> : null}
             <button className="border-0 bg-transparent p-0 d-flex align-items-center" style={{ color: "var(--dark)", opacity: ".7" }} onClick={() => setModal("slipage")}>
               <svg xmlns="http://www.w3.org/2000/svg" width={"20px"} fill="currentcolor" height={"20px"} viewBox="0 0 512 512"><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" /></svg>
             </button>
@@ -496,8 +496,8 @@ const TradeContainer = ({ tokenData, getHolderDistribution, getTokenChartDetail,
           <div className={style.btn_group+" d-flex gap-2 mt-2"} >
             <button className="px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat(formatEther(tokenBalance) * 0.25).toFixed(18))} >25 %</button>
             <button className=" px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat(formatEther(tokenBalance) * 0.50).toFixed(18))}>50 %</button>
-            <button className=" px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat(formatEther(tokenBalance) * 2).toFixed(18))}>75 %</button>
-            <button className=" px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat(formatEther(tokenBalance) * 1).toFixed(18))}>100 %</button>
+            <button className=" px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat(formatEther(tokenBalance) * 0.75).toFixed(18))}>75 %</button>
+            <button className=" px-1" style={{ fontSize: "14px" }} onClick={() => setSellValue(parseFloat((formatEther(tokenBalance) * 1) - parseFloat("0.1")).toFixed(18))}>100 %</button>
           </div>
         }
         {buyErr && (
